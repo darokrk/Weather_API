eaabf846ae2198656cf5803cbf573973ebcb0a21
Dariusz Rosiek <daro.krk@interia.pl>
2018-09-22 22:07:25 +0200

    Create style in CSS, add image

--- js/script.js
+++ js/script.js
@@ -1,28 +1,46 @@
 var url = 'https://api.openweathermap.org/data/2.5/weather?q=';
-var city = input.value;
 var id = 'f898e87d753c1e5de2cf49473bed40c5';
-var content = document.getElementById('container');
+var content = document.getElementById('weather__box');
 var inputElem = document.getElementById('input');
 var btnSearch = document.getElementById('btn-search');
-var cityContainer = document.createElement('div');
-var cityInfo = document.createElement('p');
-var citySpan = document.createElement('span');
-
-
 
 btnSearch.addEventListener('click', weatherSearch);
 inputElem.addEventListener('keypress', function (e) {
     if (e.which == 13) {
         weatherSearch();
     }
-})
+});
+
+function weatherSearch() {
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', url + inputElem.value + '&units=metric&APPID=' + id);
+    xhr.addEventListener('load', function () {
+        if (this.status === 200) {
+            var json = JSON.parse(this.response);
+            insertData(json);
+        }
+    })
+    xhr.send();
+}
+
+function insertData(data) {
+    content.innerHTML = '';
+    cityContainer.innerHTML = '';
+    inputElem.value = '';
+    weatherDisplay(data.weather);
+    insertCityData(data);
+    insertMainData(data.main);
+    insertWindData(data.wind);
+    insertCordData(data.coord);
+}
 
 function insertCityData(data) {
-    // content.innerHTML = '';
-    inputElem.value = '';
-    cityContainer.classList.add('city-info')
-    cityInfo.classList.add('city-info__content');
-    citySpan.classList.add('city-info--span');
+    var cityContainer = document.createElement('div');
+    var cityInfo = document.createElement('p');
+    var citySpan = document.createElement('span');
+    cityContainer.classList.add('city__box');
+    cityInfo.classList.add('city__text');
+    citySpan.classList.add('city__text--span');
     citySpan.innerHTML = data.weather[0].description;
     cityInfo.innerText = data.name + ', ' + data.sys.country + ' ';
     cityInfo.appendChild(citySpan);
@@ -32,70 +50,55 @@ function insertCityData(data) {
 }
 
 function weatherDisplay(data) {
-    var iconContainer = document.createElement('div');
-    iconContainer.classList.add('icon__content');
+    var iconBox = document.createElement('div');
+    iconBox.classList.add('icon__box');
     var img = document.createElement('img');
+    img.classList.add('icon__img');
     img.src = 'https://openweathermap.org/img/w/' + data[0].icon + '.png';
-    iconContainer.appendChild(img);
-    content.appendChild(iconContainer);
+    iconBox.appendChild(img);
+    content.appendChild(iconBox);
 }
 
 function insertMainData(data) {
     Object.keys(data).forEach(function (key, i) {
-        var paragraph = document.createElement('p');
-        paragraph.classList.add('temp-info');
+        var mainParagraph = document.createElement('p');
+        mainParagraph.classList.add('temp__info');
         if (i === 0) {
             var tempSpan = document.createElement('span');
-            tempSpan.classList.add('temp-info--span');
-            var temp = document.createTextNode(data[key] + '°C');
+            tempSpan.classList.add('temp__info--span');
+            var temp = document.createTextNode(data[key] + ' °C');
             tempSpan.appendChild(temp);
-            paragraph.appendChild(tempSpan);
-            cityContainer.appendChild(paragraph);
+            mainParagraph.appendChild(tempSpan);
+            cityContainer.appendChild(mainParagraph);
         } else if (i === 1) {
             var pressure = document.createTextNode(key + ' ' + data[key] + ' hpa');
-            paragraph.appendChild(pressure);
-            cityContainer.appendChild(paragraph);
+            mainParagraph.appendChild(pressure);
+            cityContainer.appendChild(mainParagraph);
         } else if (i === 2) {
             var humidity = document.createTextNode(key + ' ' + data[key] + ' %');
-            paragraph.appendChild(humidity);
-            cityContainer.appendChild(paragraph);
+            mainParagraph.appendChild(humidity);
+            cityContainer.appendChild(mainParagraph);
         }
     })
 }
 
 function insertWindData(data) {
     var windParagraph = document.createElement('p');
-    windParagraph.classList.add('wind-info');
+    windParagraph.classList.add('wind__info');
     var wind = document.createTextNode('wind ' + data.speed + ' m/s');
     windParagraph.appendChild(wind);
     cityContainer.appendChild(windParagraph);
 }
 
 function insertCordData(data) {
+    var coordParagraph = document.createElement('p');
+    coordParagraph.innerHTML = 'coords';
     var coordLink = document.createElement('a');
-    coordLink.classList.add('coord-link');
-    var linkText = document.createTextNode('coords ' + data.lat + ',' + data.lon);
+    coordLink.classList.add('coord__link');
+    var linkText = document.createTextNode(data.lat + ',' + data.lon);
     coordLink.href = 'https://openweathermap.org/weathermap?zoom=12&lat=' + data.lat + '&lon=' + data.lon;
     coordLink.target = '_blank';
     coordLink.appendChild(linkText);
-    cityContainer.appendChild(coordLink);
-}
-
-var xhr = new XMLHttpRequest();
-
-function weatherSearch() {
-    var inputValue = document.getElementById('input').value;
-    xhr.open('GET', url + inputValue + '&units=metric' + '&APPID=' + id);
-    xhr.addEventListener('load', function () {
-        if (this.status === 200) {
-            var json = JSON.parse(this.response);
-            console.log(json);
-            weatherDisplay(json.weather);
-            insertCityData(json);
-            insertMainData(json.main);
-            insertWindData(json.wind);
-            insertCordData(json.coord);
-        }
-    })
-    xhr.send();
+    coordParagraph.appendChild(coordLink);
+    cityContainer.appendChild(coordParagraph);
 }
